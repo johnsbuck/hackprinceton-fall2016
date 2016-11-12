f3a9e3bde54e30fabe25889a991d4d5d9bcf6109
John Bucknam <jbuck594@gmail.com>
2016-11-11 23:20:48 -0500

    Added d3 and data

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+
+gulp.task('copy', function() {
+  gulp.src(['node_modules/d3/build/d3.js', 'node_modules/d3/build/d3.min.js'])
+    .pipe(gulp.dest('public/vendor/d3'))
+});
+
+gulp.task('default', [,'copy']);
